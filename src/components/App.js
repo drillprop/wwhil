0386d6a7645ebc79d2b22a8a11ca6380fa76eedd
drillprops <drylbartosz@gmail.com>
2019-06-14 23:21:25 +0200

    apply GlobalStyle to app

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import Router from './Router';
+import { GlobalStyle } from '../utils/GlobalStyles';
 
 const App = () => {
-  return <Router />;
+  return (
+    <>
+      <GlobalStyle />
+      <Router />
+    </>
+  );
 };
 
 export default App;
