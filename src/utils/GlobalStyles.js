b72f7e7aa37bf5f38a21abf73c25520e590ef67f
drillprops <drylbartosz@gmail.com>
2019-06-14 23:21:05 +0200

    create GlobalStyle and import two fonts from google fonts

---
+++ src/utils/GlobalStyles.js
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css?family=Merriweather:300,400,400i,700i|Muli:200,400&display=swap');
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: 'Merriweather', serif;    
+  }
+
+`;
