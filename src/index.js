b78a201b89884f61d4d3f2059c1520b8d7a849b9
drillprops <drylbartosz@gmail.com>
2019-06-16 19:14:28 +0200

    wrap entire application with BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import { BrowserRouter } from 'react-router-dom';
 
 const app = document.getElementById('app');
-ReactDOM.render(<App />, app);
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  app
+);
