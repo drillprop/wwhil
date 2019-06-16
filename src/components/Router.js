dbe7a664eb4b660cdf3fb5999d8ca92564af3195
drillprops <drylbartosz@gmail.com>
2019-06-16 19:35:14 +0200

    create transition between routes

--- src/components/Router.js
+++ src/components/Router.js
@@ -3,16 +3,32 @@ import { Route, Switch, __RouterContext } from 'react-router-dom';
 import Quote from './Quote';
 import Title from './Title';
 import Text from './Text';
+import { useTransition, animated } from 'react-spring';
 
 const Router = () => {
-  const context = useContext(__RouterContext);
-  console.log(context);
+  const { location } = useContext(__RouterContext);
+  const animation = {
+    from: { opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 }
+  };
+  const transitions = useTransition(
+    location,
+    location => location.pathname,
+    animation
+  );
   return (
-    <Switch>
-      <Route path='/' exact component={Quote} />
-      <Route path='/title/' component={Title} />
-      <Route path='/text/' component={Text} />
-    </Switch>
+    <>
+      {transitions.map(({ item, props, key }) => (
+        <animated.div style={props} key={key}>
+          <Switch location={item}>
+            <Route path='/' exact component={Quote} />
+            <Route path='/title' component={Title} />
+            <Route path='/text' component={Text} />
+          </Switch>
+        </animated.div>
+      ))}
+    </>
   );
 };
 
