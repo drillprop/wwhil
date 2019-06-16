988a1beea09b155d22e9c0a4e0aa3f22f999ea77
drillprops <drylbartosz@gmail.com>
2019-06-16 19:13:56 +0200

    replace BrowserRouter with Switch

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Quote from './Quote';
 import Title from './Title';
 import Text from './Text';
 
 const Router = () => {
   return (
-    <BrowserRouter>
+    <Switch>
       <Route path='/' exact component={Quote} />
       <Route path='/title/' component={Title} />
       <Route path='/text/' component={Text} />
-    </BrowserRouter>
+    </Switch>
   );
 };
 
