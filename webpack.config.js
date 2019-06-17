902e914e1a5ffabe35c9c9366df9ef5477bd6437
drillprops <drylbartosz@gmail.com>
2019-06-17 21:01:12 +0200

    set up and install loader for images

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,10 @@ module.exports = {
       {
         test: /\.css$/,
         use: [{ loader: 'style-loader' }, { loader: 'css-loader' }]
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/,
+        use: ['file-loader']
       }
     ]
   },
