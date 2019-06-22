2abf920bcce52a4bdd4abe860d1f209205d47976
drillprops <drylbartosz@gmail.com>
2019-06-22 12:19:42 +0200

    on production change settings for image-webpack-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,58 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.m?js$/,
-        exclude: /(node_modules)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-react']
-          }
-        }
-      },
-      {
-        test: /\.css$/,
-        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }]
-      },
-      {
-        test: /\.(png|jpe?g|gif)$/,
-        use: [
-          'file-loader',
-          {
-            loader: 'image-webpack-loader',
+module.exports = function(env, argv) {
+  console.log(argv.mode);
+  return {
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'main.js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules)/,
+          use: {
+            loader: 'babel-loader',
             options: {
-              mozjpeg: {
-                progressive: true,
-                quality: 65
-              }
+              presets: ['@babel/preset-env', '@babel/preset-react']
             }
           }
-        ]
-      }
+        },
+        {
+          test: /\.css$/,
+          use: [{ loader: 'style-loader' }, { loader: 'css-loader' }]
+        },
+        {
+          test: /\.(png|jpe?g|gif)$/,
+          use: [
+            'file-loader',
+            {
+              loader: 'image-webpack-loader',
+              options: {
+                bypassOnDebug: argv.mode !== 'production' && true,
+                disable: argv.mode !== 'production' && true,
+                mozjpeg: {
+                  progressive: true,
+                  quality: 65
+                }
+              }
+            }
+          ]
+        }
+      ]
+    },
+    devServer: {
+      historyApiFallback: true,
+      overlay: true
+    },
+    stats: 'minimal',
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: 'index.html'
+      })
     ]
-  },
-  devServer: {
-    historyApiFallback: true,
-    overlay: true
-  },
-  stats: 'minimal',
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'index.html'
-    })
-  ]
+  };
 };
