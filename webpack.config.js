7e3d36984f5f0d04b64a29c40a4e8de887146d9f
drillprops <drylbartosz@gmail.com>
2019-09-03 13:00:44 +0200

    setup gh pages

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,8 @@ module.exports = function(env, argv) {
     entry: './src/index.js',
     output: {
       path: path.resolve(__dirname, 'dist'),
-      filename: 'main.js'
+      filename: 'main.js',
+      publicPath: './'
     },
     module: {
       rules: [
